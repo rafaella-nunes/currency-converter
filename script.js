d7357dbd0775ce9c6c274245042ebf2f542d2022
rafaella-nunes <faellanunes01@gmail.com>
2023-09-27 11:25:34 -0300

    chore: format returned value

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ form.addEventListener('submit', handleSubmit);
 const inputValue = document.getElementById('value-usd');
 const selectedCurrency = document.getElementById('currency');
 const result = document.getElementById('result');
+let convertedValue = 0;
 
 function handleSubmit(e){
     e.preventDefault();
@@ -15,4 +16,26 @@ function handleSubmit(e){
         alert('Escolha uma moeda.');
         return;
     }
-}
+
+    converter();
+};
+
+function converter(){
+    if (selectedCurrency.value === 'eur'){
+        convertedValue = inputValue.value * 0.95;
+        result.innerHTML = valueFormater('USD', 'EUR');
+    }else if(selectedCurrency.value === 'brl'){
+        convertedValue = inputValue.value * 5.02;
+        result.innerHTML = valueFormater('USD', 'BRL');
+    }else if(selectedCurrency.value === 'krw'){
+        convertedValue = inputValue.value * 1354.39;
+        result.innerHTML = valueFormater('USD', 'KRW');
+    }
+    inputValue.value = '';
+    selectedCurrency.value = '';
+};
+
+function valueFormater(locale, currency){
+    const value = convertedValue.toLocaleString(`${locale}`, {style: 'currency', currency: `${currency}`});
+    return `<span>💲</span> ${value} <span>💲</span> `;
+};
